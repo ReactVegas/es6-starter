9d54b756527b22d1d50cac261070b0c9c49f75e8
Andrew McCloud <andrew.mccloud@formsdirect.net>
2015-06-26 17:25:48 -0700

    Add bind shim to fix CI test

--- config/karma.ci.js
+++ config/karma.ci.js
@@ -6,6 +6,10 @@ export default function(config) {
   defaultConfig(config);
   config.set({
     singleRun: true,
-    browsers: ['PhantomJS']
+    browsers: ['PhantomJS'],
+    // Need to use phantomjs-shim for Function.prototype.bind until
+    // karama-phantomjs-launcher upgrades to PhantomJS 2.0.
+    // https://github.com/karma-runner/karma-phantomjs-launcher/issues/56
+    frameworks: ['phantomjs-shim'].concat(config.frameworks)
   });
 }
